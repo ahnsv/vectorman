8d8f7e2fd377f99b4fbe9c9e4fa2c7c074b6b5a0
ahnsv <ahnsv@bc.edu>
2023-04-30 17:56:46 +0900

    feat: add swagger

--- main.go
+++ main.go
@@ -6,42 +6,45 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // On-call schedule entity
 type OnCallSchedule struct {
-	ID       int
-	Start    time.Time
-	End      time.Time
-	Rotation []int // Array of on-call personnel IDs in rotation order
-	TimeZone string
+	ID       int       `json:"id"`
+	Start    time.Time `json:"start" binding:"required"`
+	End      time.Time `json:"end" binding:"required"`
+	Rotation []int     `json:"rotation" binding:"required"`
+	TimeZone string    `json:"timezone" binding:"required"`
 }
 
 // On-call personnel entity
 type OnCallPersonnel struct {
-	ID           int
-	Name         string
-	Phone        string
-	Email        string
-	NotifyMethod string // SMS, email, phone call
+	ID           int    `json:"id" binding:"required"`
+	Name         string `json:"name" binding:"required"`
+	Phone        string `json:"phone" binding:"required"`
+	Email        string `json:"email" binding:"required"`
+	NotifyMethod string `json:"notify_method" binding:"required"`
 }
 
 // Incident entity
 type Incident struct {
-	ID          int
-	Severity    string // low, medium, high
-	Description string
-	Timestamp   time.Time
+	ID          int       `json:"id" binding:"required"`
+	Severity    string    `json:"severity" binding:"required"` // low, medium, high
+	Description string    `json:"description" binding:"required"`
+	Status      string    `json:"status" binding:"required"` // open, closed
+	Timestamp   time.Time `json:"timestamp" binding:"required"`
 }
 
 // Notification entity
 type Notification struct {
-	ID           int
-	IncidentID   int
-	PersonnelID  int
-	Severity     string
-	NotifyMethod string
-	Timestamp    time.Time
+	ID           int       `json:"id" binding:"required"`
+	IncidentID   int       `json:"incident_id" binding:"required"`
+	PersonnelID  int       `json:"personnel_id" binding:"required"`
+	Severity     string    `json:"severity" binding:"required"`
+	NotifyMethod string    `json:"notify_method" binding:"required"`
+	Timestamp    time.Time `json:"timestamp" binding:"required"`
 }
 
 // On-call schedule aggregate root
@@ -67,11 +70,7 @@ var notifications []Notification
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+
 	// Define endpoint for creating a new On-call schedule
 	r.POST("/oncall/schedule", func(c *gin.Context) {
 		var newSchedule OnCallSchedule
@@ -128,21 +127,132 @@ func main() {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Schedule not found"})
 	})
 
+	r.POST("/oncall/personnel", func(c *gin.Context) {
+		var newPersonnel OnCallPersonnel
+		err := c.BindJSON(&newPersonnel)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+			return
+		}
+		onCallPersonnel = append(onCallPersonnel, newPersonnel)
+		c.JSON(http.StatusCreated, gin.H{"status": "OK"})
+	})
 	r.GET("/oncall/personnel", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "personnels",
-		})
+		c.JSON(http.StatusOK, onCallPersonnel)
 	})
-	r.GET("/incident", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "incidents",
-		})
+	r.PUT("/oncall/personnel/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+			return
+		}
+		var updatedPersonnel OnCallPersonnel
+		err = c.BindJSON(&updatedPersonnel)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+			return
+		}
+		for i := range onCallPersonnel {
+			if onCallPersonnel[i].ID == id {
+				onCallPersonnel[i] = updatedPersonnel
+				c.JSON(http.StatusOK, gin.H{"status": "OK"})
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "Personnel not found"})
 	})
+	r.GET("/oncall/personnel/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+			return
+		}
+		for _, personnel := range onCallPersonnel {
+			if personnel.ID == id {
+				c.JSON(http.StatusOK, personnel)
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "Personnel not found"})
+	})
+	r.DELETE("/oncall/personnel/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+			return
+		}
+		for i, personnel := range onCallPersonnel {
+			if personnel.ID == id {
+				onCallPersonnel = append(onCallPersonnel[:i], onCallPersonnel[i+1:]...)
+				c.JSON(http.StatusOK, gin.H{"status": "OK"})
+				return
+			}
+		}
+	})
+
+	// Define endpoint for creating a new incident
+	r.POST("/incidents", func(c *gin.Context) {
+		var newIncident Incident
+		if err := c.ShouldBindJSON(&newIncident); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		newIncident.ID = len(incidents) + 1
+		newIncident.Status = "Open"
+		incidents = append(incidents, newIncident)
+		c.JSON(http.StatusCreated, newIncident)
+	})
+
+	// Define endpoint for retrieving a specific incident by ID
+	r.GET("/incidents/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		for _, incident := range incidents {
+			if strconv.Itoa(incident.ID) == id {
+				c.JSON(http.StatusOK, incident)
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "Incident not found"})
+	})
+
+	// Define endpoint for updating an existing incident
+	r.PUT("/incidents/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		for i, incident := range incidents {
+			if strconv.Itoa(incident.ID) == id {
+				var updatedIncident Incident
+				if err := c.ShouldBindJSON(&updatedIncident); err != nil {
+					c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+					return
+				}
+				updatedIncident.ID = incident.ID
+				incidents[i] = updatedIncident
+				c.JSON(http.StatusOK, updatedIncident)
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "Incident not found"})
+	})
+
+	// Define endpoint for deleting an existing incident
+	r.DELETE("/incidents/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		for i, incident := range incidents {
+			if strconv.Itoa(incident.ID) == id {
+				incidents = append(incidents[:i], incidents[i+1:]...)
+				c.JSON(http.StatusOK, gin.H{"status": "OK"})
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "Incident not found"})
+	})
+
 	r.GET("/notification", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "notifications",
 		})
 	})
+
 	r.GET("/notification/personnel", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "notification personnels",
@@ -153,5 +263,7 @@ func main() {
 			"message": "incidents",
 		})
 	})
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
